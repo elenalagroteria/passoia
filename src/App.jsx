a3a07b534ddb0e9494afbe01ecb8a80875930356
elenalagroteria <elenalagroteria@gmail.com>
2025-11-18 13:53:19 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import Header from "./components/1Header";
 import Banner from "./components/2Banner"
 import Looks from "./components/3Looks";
 import Lancamentos from "./components/4Lancamentos";
-import Novidades from "./components/5Novidades";
-import Footer from "./components/6Footer";
 
 function App() {
   return (
