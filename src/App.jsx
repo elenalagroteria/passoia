765e91860223dfab362bea3633b4971f6a8e4284
elenalagroteria <elenalagroteria@gmail.com>
2025-11-19 13:44:28 -0300

    componentes ok

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Looks from "./components/3Looks";
 import Lancamentos from "./components/4Lancamentos";
 import Novidades from "./components/5Novidades";
 import Footer from "./components/6Footer";
-import './styles/globals.scss';
+import './styles/global.scss';
 
 function App() {
   return (
