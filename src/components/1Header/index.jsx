dfea3b118dd4274fdc359b555b75521882310522
elenalagroteria <elenalagroteria@gmail.com>
2025-11-21 23:20:16 -0300

    finalizando com menu sanduíche e favicon

--- src/components/1Header/index.jsx
+++ src/components/1Header/index.jsx
@@ -1,18 +1,31 @@
 import "./header.scss";
 import logo from "../../assets/logo.png";
+import { useState } from 'react'
 
 function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <header>
       <img src={logo} alt="logo" />
 
-      <ul>
+      <ul className={`menuSanduiche ${showMenu ? "show" : ""}`} onClick={toggleMenu}>
         <li>LOOKS</li>
         <li>LANÇAMENTOS</li>
         <li>NOVIDADES</li>
       </ul>
+
+      <div className="menuButton" onClick={toggleMenu}>
+        <span className="linha"></span>
+        <span className="linha"></span>
+        <span className="linha"></span>
+      </div>
     </header>
-    
   );
 }
+
 export default Header;
